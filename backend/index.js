e463ab7475779f5b5c76c3c26dedca7521650a0c
nittin-dev <nittinjr29@gmail.com>
2023-10-28 09:05:34 +0530

    create test api route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const app = express();
+import router from './routes/user.routes.js';
 mongoose.connect("mongodb://localhost:27017/").then(()=>{
     console.log('mongodb connected')
 }).catch((err)=>{
     console.log(err.message);
 })
+const app = express();
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
 })
+app.use('/api/users',router);
